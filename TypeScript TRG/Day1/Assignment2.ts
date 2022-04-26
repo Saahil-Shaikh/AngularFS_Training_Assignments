12a48bfbe141d2de1b431fb97dc01c5b00f90d1c
Saahil-Shaikh <57488507+Saahil-Shaikh@users.noreply.github.com>
2022-04-26 14:58:47 +0530

    Added Assignment2.ts

---
+++ TypeScript TRG/Day1/Assignment2.ts
@@ -0,0 +1,72 @@
+var product_name:string = "iPhone 12";
+var unit_price:number = 50000;
+var quantity:number = 10;
+enum discount_type {HAPPY20 = 0.2, SUPER50 = 0.5};
+enum delivery_mode {Home = 30, Self = 0};
+
+function process_order(product_name, unit_price, quantity, coupon:string, delivery:string) : void {
+
+    console.log("\n")
+
+    switch(coupon){
+
+        case "HAPPY20" : {
+
+            if(delivery == "Home"){
+                console.log("Order Details\n------------------");
+                console.log("Product Name : "+product_name);
+                console.log("Product Price : "+unit_price);
+                console.log("Quantity : "+quantity);
+                let total:number = (unit_price*quantity);
+                total = total - total * discount_type["HAPPY20"] + delivery_mode["Home"];
+                console.log("Total Price after applying HAPPY20 : "+ total);
+                console.log("Deliver Mode : Home Delivery");
+            }
+            else{
+                console.log("Order Details\n------------------");
+                console.log("Product Name : "+product_name);
+                console.log("Product Price : "+unit_price);
+                console.log("Quantity : "+quantity);
+                let total:number = unit_price*quantity;
+                total = total - total * discount_type["HAPPY20"];
+                console.log("Total Price after applying HAPPY20 : "+ total);
+                console.log("Deliver Mode : Self Pickup");
+            }
+            break;
+        }
+
+        case "SUPER50" : {
+            if(delivery == "Home"){
+                console.log("Order Details\n------------------");
+                console.log("Product Name : "+product_name);
+                console.log("Product Price : "+unit_price);
+                console.log("Quantity : "+quantity);
+                let total:number = unit_price*quantity;
+                total = total - total * discount_type["SUPER50"] + delivery_mode["Home"];
+                console.log("Total Price after applying SUPER50 : "+ total);
+                console.log("Deliver Mode : Home Delivery");
+            }
+            else{
+                console.log("Order Details\n------------------");
+                console.log("Product Name : "+product_name);
+                console.log("Product Price : "+unit_price);
+                console.log("Quantity : "+quantity);
+                let total:number = unit_price*quantity;
+                total = total - total * discount_type["SUPER50"];
+                console.log("Total Price after applying SUPER50 : "+ total);
+                console.log("Deliver Mode : Self Pickup");
+            }
+            break;
+        }
+
+        default : {
+            console.log("\nIncorrect Coupon Code");
+            break;
+        }
+    }    
+
+}
+
+process_order(product_name, unit_price, quantity, "SUPER50","Home");
+process_order(product_name, unit_price, quantity, "HAPPY20","Self");
+process_order(product_name, unit_price, quantity, "NEW100","Self");

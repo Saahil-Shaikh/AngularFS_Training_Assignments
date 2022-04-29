950b2e4d44d56c584de8fb964881acf298fb0de4
Saahil-Shaikh <57488507+Saahil-Shaikh@users.noreply.github.com>
2022-04-29 11:20:39 +0530

    App component files

---
+++ Angular TRG/Day2/Assignment1/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Assignment-2';
+
+  cat1:string="Gaming";
+  cat2:string="Appliance";
+  cat3:string="Fashion";
+  cat4:string="Mobile";
+
+}
+
